dbff7476bfc4a9abc35467d2e2ff629e829b2793
Luciano de Sousa Lopes <lucianodesousalopes.github@gmail.com>
2024-09-17 07:45:50 -0300

    feat: created the first vesion of the project

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+
+const matches: string[][] = fs
+  .readFileSync("football.csv", { encoding: "utf-8" })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (
+    (match[1] === "Man United" && match[5] === "H") ||
+    (match[2] === "Man United" && match[5] === "A")
+  ) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man united won ${manUnitedWins} games`);
